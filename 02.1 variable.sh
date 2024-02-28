2294672db46ed698d0ee947dff222fcb4a0e5d1f
Theja1995 <saithejageelapaturu@gmail.com>
2024-02-28 22:30:11 +0530

    practice

---
+++ 02.1 variable.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+samayam=$(date)
+
+echo "date is ${samayam}"
--- var.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-s=charan
-c=sai
-
-echo "$s: Namaste $c anna , manchigunnava"
-echo "$c: Manchigunna le thi"
-echo "$s: Yendhi itlocchinav , em katha"
-echo "$c: Ye oke vacchina."
